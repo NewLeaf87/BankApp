7f6e5ef6470d190c6b380359332dc6e86597f49f
larell mcbride <mcbridelarell@gmail.com>
2020-05-24 19:35:44 -0400

    Final commit

---
+++ BankAccountApp.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class BankAccountApp {
+    public static void main(String[] args) {
+        List<Account>accounts = new LinkedList<Account>();  
+        
+
+        
+      
+        //Reads a CSV file 
+        String file = "original.csv";
+        List<String[]> newAccountHolders = CSV.read(file);
+        for(String[]accountHolder : newAccountHolders){
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+            if(accountType.equals("Savings")) {
+                accounts.add(new Savings(name, sSN, initDeposit));
+            }
+            else if (accountType.equals("Checking")){
+                accounts.add(new Checking(name, sSN, initDeposit));
+            }
+            else {
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
+        }
+
+        
+
+        for (Account acc : accounts){
+            System.out.println("***************");
+            acc.showInfo();
+        }
+    }
+}
